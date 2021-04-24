2c823c5d42961e47d7acb8ba02f69307047492b2
Maicon Pereira <maicon.cp@outlook.com>
2021-04-24 16:28:59 -0300

    finish

--- src/index.js
+++ src/index.js
@@ -10,19 +10,70 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if(!user) {
+    return response.status(404).json(({error: 'Username not found'}));
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro === false && user.todos.length >= 10) {
+    return response.status(403)
+  }
+
+  if((user.pro === false && user.todos.length < 10) | user.pro === true ) {
+    return next();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if(!user) {
+    return response.status(404).json(({error: 'Username not found'}));
+  }
+
+  const isValid = validate(id);
+  if(!isValid) {
+    return response.status(400);
+  }
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+  if(todoIndex === -1) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user) {
+    return response.status(404).json(({error: 'User not found'}));
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
